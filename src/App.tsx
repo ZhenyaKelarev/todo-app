7f703eed8593c4bfd3f9dbce5e05bffb60ea3d93
ZhenyaKelarev <zeka416351@gmail.com>
2025-06-02 15:45:12 +0300

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Board } from './components/Board'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">📝 Todo Board</h1>
+      <Board />
+    </div>
+  )
+}
+
+export default App
